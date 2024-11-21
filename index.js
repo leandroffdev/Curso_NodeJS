447fe7fd0e93c26f3c493caa2784cabc9dee2a8b
leandroffdev <leandrofrancagti@gmail.com>
2024-11-21 14:26:09 -0300

    express

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ app.get('/', (req, res) => {
     return res.status(200).json('Olá mundo');
 });
 
+app.get('/users/:id', (req, res) => {
+    return res.status(200).json(req.params.id);
+});
+
 app.get("/users", (req, res) => {
-    return res.status(200).json("Pagina Users")
-})
+    console.log(req.query);
+    // return res.status(200).json(req.query.usuario);
+    return res.status(200).json({ 
+        sensor:"Temperatura",
+        valor: 25.5,
+        local:"Praia"
+    });
+
+});
 
 app.listen(3000, () => {
     console.log("Server running on port", 3000);
